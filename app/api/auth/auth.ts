c1449ee3df9f3dc17620c1da918800822d4b0212
yuyainoue <yuyatuta@icloud.com>
2024-01-27 15:25:42 +0900

    add Header

--- app/api/auth/auth.ts
+++ app/api/auth/auth.ts
@@ -2,9 +2,7 @@ import NextAuth from "next-auth/next";
 import { githubProviders } from "../common/providersOptions";
 
 export const authOptions = {
-    providers: [
-        githubProviders,
-    ],
+    providers: githubProviders
 }
 
 export default NextAuth(authOptions);
